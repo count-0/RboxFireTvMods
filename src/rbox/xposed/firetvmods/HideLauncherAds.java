7972e0d2b6f452b087b6bb89d6009a31deb83f0b
WheelchairArtist <WheelchairArtist@users.noreply.github.com>
2016-07-11 14:33:28 +0200

    fix HideLauncherAds
    not the best way but it works for now. 
    as side effect we still have the placeholder as leftover and can scroll to it. not so good! 
    further we should make this optional since we already introduced a setting for it.
    
    funny thing is, even though every thing works i don't get any output from XposedBridge.log if i put it in the hook?!

--- src/rbox/xposed/firetvmods/HideLauncherAds.java
+++ src/rbox/xposed/firetvmods/HideLauncherAds.java
@@ -1,28 +1,25 @@
 package rbox.xposed.firetvmods;
 
+import android.os.Parcel;
+
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
-public class HideLauncherAds implements IXposedHookLoadPackage
-{
+public class HideLauncherAds implements IXposedHookLoadPackage{
+
 	@Override
 	public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable
 	{
-		if (!lpparam.packageName.equals("com.amazon.tv.launcher"))
-			return;
-
-		Class<?> CacheableObject = XposedHelpers.findClass("com.amazon.tv.mediabrowse.model.CacheableObject", lpparam.classLoader);
-		Class<?> ScreenChannelUpdateCallback = XposedHelpers.findClass("com.amazon.tv.launcher.ui.carousel.fragment.MediaFragment2D$ScreenChannelUpdateCallback", lpparam.classLoader);
-		XposedHelpers.findAndHookMethod("com.amazon.tv.launcher.ui.carousel.fragment.MediaFragment2D", lpparam.classLoader, "onChannelReturned", String.class, int.class, CacheableObject, ScreenChannelUpdateCallback, new XC_MethodHook() {
-			@Override
-			protected void beforeHookedMethod(MethodHookParam param) throws Throwable
-			{
-				// If the banner ad channel was returned, override the function so it doesn't add it
-				if (param.args[0].equals("[reftype=ad/banner/kso,refid=Launcher.Home.Hero]"))
-					param.setResult(null);
-			}
-		});
+		if (lpparam.packageName.equals("com.amazon.kso.blackbird"))
+		{
+			XposedHelpers.findAndHookMethod("com.amazon.kso.blackbird.service.StaticImageAd", lpparam.classLoader, "writeToParcelImpl", Parcel.class, int.class, new XC_MethodHook() {
+				@Override
+				protected void beforeHookedMethod(MethodHookParam param) throws Throwable
+				{
+						param.setResult(null);
+				}
+			});
+		}
 	}
-}
